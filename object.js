03a06618e194deca7223c47a51399d4f74f82651
Saksham <sakshammadhwal0013@gmail.com>
2026-03-30 11:32:11 +0530

    Initial commit

---
+++ object.js
@@ -0,0 +1,9 @@
+function person(first,last,age,eye){
+    this.firstName=first;
+    this.lastname=last;
+    this.age=age;
+    this.age=age;
+}
+const myfather = new person("jhon,"doli",60,"black");
+const mymother = new person("alen"doli",56,"black");
+const mysister = new person("zen,"doli",24,"black");
