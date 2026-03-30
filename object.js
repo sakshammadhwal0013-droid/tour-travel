f4f4274faac4a5580477f78ab103476a50dcced2
Saksham <sakshammadhwal0013@gmail.com>
2026-03-30 11:39:07 +0530

    Removed file from git but kept locally

--- object.js
+++
@@ -1,9 +0,0 @@
-function person(first,last,age,eye){
-    this.firstName=first;
-    this.lastname=last;
-    this.age=age;
-    this.age=age;
-}
-const myfather = new person("jhon,"doli",60,"black");
-const mymother = new person("alen"doli",56,"black");
-const mysister = new person("zen,"doli",24,"black");
